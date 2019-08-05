a4483a97b705770437d33832415fb8c7e31ab0b6
wy625571185 <625571185@qq.com>
2019-08-05 11:56:19 +0800

    添加上下午

--- YUClock/YUFoldClock/YUFoldClock.m
+++ YUClock/YUFoldClock/YUFoldClock.m
@@ -11,6 +11,9 @@
 
 @interface YUFoldClock ()
 
+/*! NOTE: 上午/下午 */
+@property (nonatomic, strong) UILabel *alertLabel;
+
 /*! NOTE: 时 */
 @property (nonatomic, strong) YUFoldClockItem *hourItem;
 
@@ -34,6 +37,8 @@
 - (void)buildUI {
     self.backgroundColor = [UIColor blackColor];
     
+    [self addSubview:self.alertLabel];
+    
     [self addSubview:self.hourItem];
     
     [self addSubview:self.minuteItem];
@@ -43,9 +48,11 @@
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    CGFloat margin = 0.07 * CGRectGetWidth(self.bounds);
+    CGFloat margin = 0.03 * CGRectGetWidth(self.bounds);
     CGFloat itemW = (CGRectGetWidth(self.bounds) - 4 * margin) / 3.0f;
     CGFloat itemY = (CGRectGetHeight(self.bounds) - itemW) / 2.0f;
+    
+    self.alertLabel.frame = CGRectMake(margin + 5, itemY - 30, 80, 30);
     
     self.hourItem.frame = CGRectMake(margin, itemY, itemW, itemW);
     self.minuteItem.frame = CGRectMake(CGRectGetMaxX(self.hourItem.frame) + margin, itemY, itemW, itemW);
@@ -57,6 +64,15 @@
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSUInteger unitFlags = NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
     NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:date];
+    
+    /*! NOTE: 设置上下午 */
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale=[[NSLocale alloc]initWithLocaleIdentifier:@"en_US"];
+    [dateFormatter setDateFormat:@"h:mm:ss-a"];
+    NSString *alertStr = [[[dateFormatter stringFromDate:[NSDate date]] componentsSeparatedByString:@"-"] lastObject];
+    self.alertLabel.text = alertStr;
+    
+    
     _hourItem.time = dateComponent.hour;
     _minuteItem.time = dateComponent.minute;
     _secondItem.time = dateComponent.second;
@@ -75,6 +91,16 @@
 }
 
 #pragma mark ========= lazy load =========
+/*! NOTE: 上午/下午 */
+-(UILabel *)alertLabel {
+    if (!_alertLabel) {
+        _alertLabel = [[UILabel alloc] init];
+        _alertLabel.textColor = [UIColor colorWithRed:186/255.0f green:183/255.0f blue:186/255.0f alpha:1];
+        _alertLabel.font = [UIFont fontWithName:@"AmericanTypewriter-CondensedBold" size:20];
+    }
+    return _alertLabel;
+}
+
 /*! NOTE: 时 */
 -(YUFoldClockItem *)hourItem {
     if (!_hourItem) {
